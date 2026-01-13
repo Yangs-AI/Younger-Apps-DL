102c7132dc05e6082e272d816cafd9ee6dcdc00c
Jason-Young-AI <AI.Jason.Young@outlook.com>
2026-01-13 17:43:28 +0800

    Now Maybe Add DAGDataset (with Metrics)

--- younger_apps_dl/datasets/graph/dag_with_metrics.py
+++ younger_apps_dl/datasets/graph/dag_with_metrics.py
@@ -6,7 +6,7 @@
 # Author: Jason Young (杨郑鑫).
 # E-Mail: AI.Jason.Young@outlook.com
 # Last Modified by: Jason Young (杨郑鑫)
-# Last Modified time: 2025-04-01 11:17:39
+# Last Modified time: 2026-01-13 17:43:01
 # Copyright (c) 2025 Yangs.AI
 # 
 # This source code is licensed under the Apache License 2.0 found in the
