285e6c19eab13e953408f0c6be433d42dcaf01a5
Jason-Young-AI <AI.Jason.Young@outlook.com>
2026-01-13 17:41:02 +0800

    Add some Design Principles comments to Datasets.

--- younger_apps_dl/datasets/graph/__init__.py
+++ younger_apps_dl/datasets/graph/__init__.py
@@ -6,12 +6,17 @@
 # Author: Jason Young (杨郑鑫).
 # E-Mail: AI.Jason.Young@outlook.com
 # Last Modified by: Jason Young (杨郑鑫)
-# Last Modified time: 2025-05-12 13:23:52
+# Last Modified time: 2026-01-02 05:04:30
 # Copyright (c) 2025 Yangs.AI
 # 
 # This source code is licensed under the Apache License 2.0 found in the
 # LICENSE file in the root directory of this source tree.
 ########################################################################
+
+
+# The graph datasets in LogicX format are implemented here.
+# They are designed to work seamlessly with the DAG-based models and generation tasks.
+# YADL only supports the processing of LogicX graph datasets (not including raw data download).
 
 
 from .dag import *
